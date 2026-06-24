0734064cdddaf64810abf0eca4ad071872047615
chinjto <contact@cyril-defaye.fr>
2026-06-24 19:48:55 +0100

    feat(page-section): Add page sections generator
    - add generator `page-section`
    - add x-prompt on all generators with args/params
    
    Closes #3 Refs #6

---
+++ src/components/page/section/index.ts
@@ -0,0 +1,15 @@
+import { Rule } from '@angular-devkit/schematics';
+import { uiComponent } from '../../../core/ui-component';
+
+interface Options {
+    page: string;
+    name: string;
+}
+
+export function pageSection(options: Options): Rule {
+    console.log("OPTIONS: ", options);
+    return uiComponent({
+        ...options,
+        type: 'section',
+    });
+}
--- src/core/ui-component/index.ts
+++ src/core/ui-component/index.ts
@@ -6,31 +6,49 @@ import {
     url,
 } from '@angular-devkit/schematics';
 import { strings } from '@angular-devkit/core';
+import {page} from "../../components/page";
 
 interface Options {
+    page?: string;
     name: string;
     type: 'page' | 'section' | 'card' | 'tile';
 }
 
 const NO_SUFFIX_TYPES = ["page"];
 
+const basePaths = {
+    page: 'src/app/pages',
+    section: 'src/app/shared/components/sections',
+    card: 'src/app/shared/components/cards',
+    tile: 'src/app/shared/components/tiles',
+};
+
 export function uiComponent(options: Options): Rule {
     return (tree, context) => {
-        const basePaths = {
-            page: 'src/app/pages',
-            section: 'src/app/shared/components/sections',
-            card: 'src/app/shared/components/cards',
-            tile: 'src/app/shared/components/tiles',
-        };
-
-        const name = strings.dasherize(options.name);
-        const className = generateClassName(options);
-        const selector = `${options.type}-${name}`;
-        const targetPath = `${basePaths[options.type]}/${name}`;
+        context.logger.info(`options: {page: "${options.page}", name: "${options.name}", type: "${options.type}"}`);
+        let filesPath;
+        let name;
+        let className;
+        let selector;
+        let targetPath;
+        if (options.page) {
+            filesPath = '../../../core/ui-component/files';
+            const page = strings.dasherize(options.page);
+            name = strings.dasherize(options.name);
+            className = generateClassName(options);
+            selector = `page-${options.page}-${options.type}-${name}`;
+            targetPath = `${basePaths["page"]}/${page}/${name}`;
+        } else {
+            filesPath = '../../core/ui-component/files';
+            name = strings.dasherize(options.name);
+            className = generateClassName(options);
+            selector = `${options.type}-${name}`;
+            targetPath = `${basePaths[options.type]}/${name}`;
+        }
 
         context.logger.info(`Generating ${options.type} "${name}" in ${targetPath}`);
 
-        const source = apply(url('../../core/ui-component/files'), [
+        const source = apply(url(filesPath), [
             applyTemplates({
                 ...strings,
                 name,
@@ -46,9 +64,16 @@ export function uiComponent(options: Options): Rule {
 }
 
 function generateClassName(options: Options): string {
+    let className = "";
     const name = strings.classify(options.name);
-    if (NO_SUFFIX_TYPES.includes(options.type)) {
-        return name;
+    className += name;
+    if (!options.page) {
+        const page = strings.classify(options.name);
+        className += page;
     }
-    return name + strings.classify(options.type);
+    if (!NO_SUFFIX_TYPES.includes(options.type)) {
+        const type = strings.classify(options.type);
+        className += type;
+    }
+    return className;
 }
